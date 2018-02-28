b1fd84ccb1887eed1ac13e8cda4d8290859a0e9b
DenisLobur <denisdenisdenis@bigmir.net>
2018-02-28 14:00:52 +0200

    add textview

--- app/src/main/java/denis/easyweather/app/ui/MainActivity.kt
+++ app/src/main/java/denis/easyweather/app/ui/MainActivity.kt
@@ -3,12 +3,18 @@ package denis.easyweather.app.ui
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import denis.easyweather.app.R
+import denis.easyweather.app.di.WeatherApplication
+import io.reactivex.disposables.CompositeDisposable
 
 class MainActivity : AppCompatActivity() {
+
+    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        WeatherApplication.appComponent.inject(this)
     }
 
 
