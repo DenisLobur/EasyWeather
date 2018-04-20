bf7d628417989926ac08e5d3dab63d14aa89f94f
DenisLobur <denisdenisdenis@bigmir.net>
2018-04-20 16:57:03 +0300

    add graph for other measures

--- app/src/main/java/denis/easyweather/app/ui/ForecastActivity.kt
+++ app/src/main/java/denis/easyweather/app/ui/ForecastActivity.kt
@@ -1,21 +1,27 @@
 package denis.easyweather.app.ui
 
+import android.graphics.DashPathEffect
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.CardView
 import android.view.LayoutInflater
 import android.widget.TextView
+import com.androidplot.util.PixelUtils
+import com.androidplot.xy.*
 import denis.easyweather.app.R
 import denis.easyweather.app.common.Util
 import denis.easyweather.app.dto.ForecastDTO
 import denis.easyweather.app.utils.Constants.FORECAST_RESPONSE
 import kotlinx.android.synthetic.main.activity_forecast.*
+import kotlinx.android.synthetic.main.item_forecast_day.*
 import kotlinx.android.synthetic.main.item_forecast_day.view.*
 import kotlinx.android.synthetic.main.label_temperature.view.*
 import me.panpf.swsv.CircularLayout
 import me.panpf.swsv.SpiderWebScoreView
 import org.parceler.Parcels
-import java.util.*
+import java.text.FieldPosition
+import java.text.Format
+import java.text.ParsePosition
 
 
 /**
@@ -75,12 +81,15 @@ class ForecastActivity : AppCompatActivity() {
         for (day in days){
             val dayView = this.layoutInflater.inflate(R.layout.item_forecast_day, horizontalLayout, false) as CardView
             dayView.date.text = Util.formatDay(day.map { it.dt_txt!! }.first()) + "\n" + Util.formatMonth(day.map { it.dt_txt!! }.first()).capitalize()
-            dayView.times.text = day.map { it -> it.dt_txt.plus(" ").plus(it.main!!.temp).plus("\n") }.toString()
-            val maxTempList = FloatArray(8)
-            day.forEachIndexed { index, it ->  maxTempList.set(index, it.main!!.tempMax!!.toFloat()) }
-            drawTemperature(maxTempList, dayView.spiderWeb_mainActivity_1, dayView.layout_mainActivity_circular1)
+            dayView.times.text = day.map { it -> it.dt_txt.plus(" ").plus(it.main!!.temp).plus(" ").plus(it.main!!.humidity).plus("\n") }.toString()
+            val tempList = FloatArray(8)
+            day.forEachIndexed { index, it ->  tempList.set(index, it.main!!.temp!!.toFloat()) }
+
+            drawTemperature(tempList, dayView.spiderWebTemp, dayView.circularTemp)
             horizontalLayout.addView(dayView)
         }
+
+        drawPlot(plot)
 
 
         val firstDayMins = firstDay.map { it.main!!.tempMin!! }.min()
@@ -112,27 +121,69 @@ class ForecastActivity : AppCompatActivity() {
 //        Log.d("description", description.toString())
     }
 
-    //TODO: Add second graph with temp_min array
-    private fun drawTemperature(tempList: FloatArray, spiderWeb_mainActivity_1: SpiderWebScoreView, layout_mainActivity_circular1: CircularLayout){
+
+    private fun drawTemperature(tempList: FloatArray, spiderWebTemp: SpiderWebScoreView, circularTemp: CircularLayout){
         val scoreArray = FloatArray(tempList.size)
-        val scoreArray2 = FloatArray(tempList.size)
         for (w in tempList.indices) {
             scoreArray[w] = tempList[w]
-            scoreArray2[w] = Random().nextFloat()
         }
-        spiderWeb_mainActivity_1.setScoreColor(this.resources.getColor(android.R.color.holo_blue_light))
-        spiderWeb_mainActivity_1.setScores(30f, scoreArray)
-        spiderWeb_mainActivity_1.setScoreColor(this.resources.getColor(android.R.color.holo_red_light))
-        spiderWeb_mainActivity_1.setScores(30f, scoreArray2)
+        spiderWebTemp.setScoreColor(this.resources.getColor(R.color.high_temp_color))
+        spiderWebTemp.setScores(scoreArray.max()!!.plus(10), scoreArray)
 
-        layout_mainActivity_circular1.removeAllViews()
+        circularTemp.removeAllViews()
         for (score in tempList) {
-            val scoreTextView = LayoutInflater.from(this).inflate(R.layout.label_temperature, layout_mainActivity_circular1, false) as TextView
+            val scoreTextView = LayoutInflater.from(this).inflate(R.layout.label_temperature, circularTemp, false) as TextView
             if(score != 0.0f) {
                 scoreTextView.label_tmp.text = score.toString()
             }
-            layout_mainActivity_circular1.addView(scoreTextView)
+            circularTemp.addView(scoreTextView)
         }
     }
+
+    private fun drawPlot(plot: XYPlot){
+        val domainLabels = arrayOf<Number>(1, 2, 3, 6, 7, 8, 9, 10, 13, 14)
+        val series1Numbers = arrayOf<Number>(1, 4, 2, 8, 4, 16, 8, 32, 16, 64)
+        val series2Numbers = arrayOf<Number>(5, 2, 10, 5, 20, 10, 40, 20, 80, 40)
+
+        // turn the above arrays into XYSeries':
+        // (Y_VALS_ONLY means use the element index as the x value)
+        val series1 = SimpleXYSeries(arrayListOf(1, 4, 2, 8, 4, 16, 8, 32, 16, 64), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series1")
+        val series2 = SimpleXYSeries(arrayListOf(5, 2, 10, 5, 20, 10, 40, 20, 80, 40), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2")
+
+        // create formatters to use for drawing a series using LineAndPointRenderer
+        // and configure them from xml:
+        val series1Format = LineAndPointFormatter(this, R.xml.line_point_formatter_with_labels)
+
+        val series2Format = LineAndPointFormatter(this, R.xml.line_point_formatter_with_labels_2)
+
+        // add an "dash" effect to the series2 line:
+        series2Format.linePaint.pathEffect = DashPathEffect(floatArrayOf(
+
+                // always use DP when specifying pixel sizes, to keep things consistent across devices:
+                PixelUtils.dpToPix(20f), PixelUtils.dpToPix(15f)), 0f)
+
+        // just for fun, add some smoothing to the lines:
+        // see: http://androidplot.com/smooth-curves-and-androidplot/
+        series1Format.interpolationParams = CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal)
+
+        series2Format.interpolationParams = CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal)
+
+        // add a new series' to the xyplot:
+        plot.addSeries(series1, series1Format)
+        plot.addSeries(series2, series2Format)
+
+        plot.graph.getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(object : Format() {
+            override fun format(obj: Any, toAppendTo: StringBuffer, pos: FieldPosition): StringBuffer {
+                val i = Math.round((obj as Number).toFloat())
+                return toAppendTo.append(domainLabels[i])
+            }
+
+            override fun parseObject(source: String, pos: ParsePosition): Any? {
+                return null
+            }
+        })
+    }
+
+
 
 }
