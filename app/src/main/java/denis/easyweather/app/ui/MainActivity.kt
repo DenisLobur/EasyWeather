55b902ad5d6db4ddb242a144775f03dd234f5966
DenisLobur <denisdenisdenis@bigmir.net>
2018-09-06 17:50:40 +0300

    start working on location

--- app/src/main/java/denis/easyweather/app/ui/MainActivity.kt
+++ app/src/main/java/denis/easyweather/app/ui/MainActivity.kt
@@ -1,10 +1,18 @@
 package denis.easyweather.app.ui
 
+import android.Manifest
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import denis.easyweather.app.R
 import denis.easyweather.app.common.Util
 import denis.easyweather.app.di.WeatherApplication
@@ -30,12 +38,39 @@ class MainActivity : AppCompatActivity() {
     private val compositeDisposable: CompositeDisposable = CompositeDisposable()
     private lateinit var viewModel: WeatherViewModel
     private val TAG: String = MainActivity::class.java.simpleName
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                        123)
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+
         WeatherApplication.appComponent.inject(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(WeatherViewModel::class.java)
 
         searchCity.setOnClickListener {
@@ -49,6 +84,24 @@ class MainActivity : AppCompatActivity() {
             setupForecastObserver(cityName)
         }
 
+        fusedLocationClient.lastLocation
+                .addOnSuccessListener { location : Location? ->
+                    // Got last known location. In some rare situations this can be null.
+                    location?.apply {
+                        Log.d(TAG, "lat: "+ latitude + "lon: " + longitude)
+                    }
+                }
+
+    }
+
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            Log.d(TAG, "lat: " + location.latitude + "lon: " + location.longitude);
+        }
+
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
     }
 
     private fun setupWeatherDetailObserver(city: String): Disposable? {
