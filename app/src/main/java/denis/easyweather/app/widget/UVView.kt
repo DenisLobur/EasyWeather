14102ddf6b66b9d1a7c70f7211d09bac88101d07
DenisLobur <denisdenisdenis@bigmir.net>
2018-10-24 11:07:29 +0300

    start rewriting UV widget

--- app/src/main/java/denis/easyweather/app/widget/UVView.kt
+++ app/src/main/java/denis/easyweather/app/widget/UVView.kt
@@ -37,25 +37,25 @@ class UVView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = 
         paintGreenCircle = Paint().apply {
             color = Color.GREEN
             style = Paint.Style.STROKE
-            strokeWidth = 60F
+            strokeWidth = 5F
         }
 
         paintYellowCircle = Paint().apply {
             color = Color.YELLOW
             style = Paint.Style.STROKE
-            strokeWidth = 60F
+            strokeWidth = 5F
         }
 
         paintOrangeCircle = Paint().apply {
             color = resources.getColor(R.color.orange)
             style = Paint.Style.STROKE
-            strokeWidth = 60F
+            strokeWidth = 5F
         }
 
         paintRedCircle = Paint().apply {
             color = Color.RED
             style = Paint.Style.STROKE
-            strokeWidth = 60F
+            strokeWidth = 5F
         }
 
         oval = RectF()
@@ -87,13 +87,13 @@ class UVView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = 
 
         canvas.apply {
             oval.apply {
-                left = width / 2 - 200F
-                top = height / 2 - 200F
-                right = width / 2 + 200F
-                bottom = height / 2 + 200F
+                left = 0f
+                top = 0f
+                right = width+1f
+                bottom = height+1f
             }
 
-            drawArc(oval, 180F, 54F, false, paintGreenCircle)
+            drawArc(oval, 120F, 54F, false, paintGreenCircle)
             drawArc(oval, 234F, 54F, false, paintYellowCircle)
             drawArc(oval, 288F, 36F, false, paintOrangeCircle)
             drawArc(oval, 324F, 36F, false, paintRedCircle)
